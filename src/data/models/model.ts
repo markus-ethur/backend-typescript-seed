07f5f3f25ffd82bfc86cf0e25182ef40c7eab4f1
Markus Ethur <markus.ethur@gmail.com>
2020-10-19 02:09:59 -0300

    temp fix for knex connection params

--- src/data/models/model.ts
+++ src/data/models/model.ts
@@ -1,5 +1,21 @@
 import { TypedKnex } from '@wwwouter/typed-knex';
-import { knexConfig } from '../../../knexfile';
+import knex from 'knex';
+
+require('dotenv').config();
+
+export const knexConfig = knex({
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    bigNumberStrings: true,
+  },
+  migrations: {
+    tableName: 'migrations',
+  },
+});
 
 export abstract class Model<T> {
   protected db: TypedKnex;
